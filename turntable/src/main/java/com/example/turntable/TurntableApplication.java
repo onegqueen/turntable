ae45ea88e1ffee4bbdb93155182a4375ecfd4521
onegqueen <rkdwldnjs30@naver.com>
2024-06-08 22:21:59 +0900

    project 생성

---
+++ turntable/src/main/java/com/example/turntable/TurntableApplication.java
@@ -0,0 +1,13 @@
+package com.example.turntable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurntableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurntableApplication.class, args);
+	}
+
+}
---
+++ turntable/src/test/java/com/example/turntable/TurntableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turntable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurntableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
