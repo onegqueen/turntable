2e37640f076abb0ff4d039553aa1a6e7b3bf3436
onegqueen <rkdwldnjs30@naver.com>
2024-06-19 15:15:01 +0900

    feat : spring securityconfig 추가

--- turntable/src/main/java/com/example/turntable/spotify/SpotifyController.java
+++ turntable/src/main/java/com/example/turntable/spotify/SpotifyController.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SpotifyController {
     @Autowired
     private SpotifyService spotifyService;
