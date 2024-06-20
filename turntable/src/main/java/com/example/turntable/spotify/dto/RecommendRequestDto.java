4a9a7f4e2bec588b7e0abe0a91f14b4794702fc2
onegqueen <rkdwldnjs30@naver.com>
2024-06-20 13:37:42 +0900

    feat : 음악 추천 요청에 담을 시드정보를 담은 dto 추가

---
+++ turntable/src/main/java/com/example/turntable/spotify/dto/RecommendRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.turntable.spotify.dto;
+
+import java.util.List;
+
+public class RecommendRequestDto {
+    private List<String> seedArtiest;
+    private List<String> seedTracks;
+    private List<String> seedGenres;
+}
