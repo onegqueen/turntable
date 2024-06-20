0e73ee17b7a89bea44b1cbb20ce4cc8919f1200b
chaewonjeong <chaewonjeong.dev@gmail.com>
2024-06-21 05:17:09 +0900

    feat: 도메인 Member 추가

--- turntable/src/main/java/com/example/turntable/domain/Member.java
+++ turntable/src/main/java/com/example/turntable/domain/Member.java
@@ -1,9 +1,24 @@
 package com.example.turntable.domain;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "back_ground_image")
+    private String backGroundImage;
 
 }
