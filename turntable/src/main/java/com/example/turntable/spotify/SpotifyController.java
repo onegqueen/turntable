d4b02a9adbf548decb2dafedab52d4dc38cf69ff
onegqueen <rkdwldnjs30@naver.com>
2024-06-20 15:33:51 +0900

    feat : 음악 추천을 보낼 post매핑 컨트롤러 메소드 추가

--- turntable/src/main/java/com/example/turntable/spotify/SpotifyController.java
+++ turntable/src/main/java/com/example/turntable/spotify/SpotifyController.java
@@ -1,10 +1,13 @@
 package com.example.turntable.spotify;
 
 import com.example.turntable.spotify.dto.ArtistResponseDto;
+import com.example.turntable.spotify.dto.RecommendRequestDto;
 import com.example.turntable.spotify.dto.TrackResponseDto;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,10 @@ public class SpotifyController {
     @GetMapping("/search/genre")
     public List<String> searchGenres() {
         return spotifyService.searchGenre();
+    }
+
+    @PostMapping("/recommendations")
+    public List<TrackResponseDto> getRecommendations(@RequestBody RecommendRequestDto recommendationsRequestDto) {
+        return spotifyService.getRecommends(recommendationsRequestDto);
     }
 }
