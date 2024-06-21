8aaa2d2fa112f2a36a057a96ca8f62ffcf49f351
chaewonjeong <chaewonjeong.dev@gmail.com>
2024-06-21 09:52:51 +0900

    feat: 도메인 DailyComment 추가

---
+++ turntable/src/main/java/com/example/turntable/domain/DailyComment.java
@@ -0,0 +1,38 @@
+package com.example.turntable.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class DailyComment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "comment", nullable = false)
+    private String comment;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
