7a198c25f157bbf0e77f0bfbdfea562401c7f2de
chaewonjeong <chaewonjeong.dev@gmail.com>
2024-06-21 10:51:23 +0900

    feat: 도메인 GuestComment

---
+++ turntable/src/main/java/com/example/turntable/domain/GuestComment.java
@@ -0,0 +1,40 @@
+package com.example.turntable.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class GuestComment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private DailyComment dailyComment;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
