579de50c6c95f10b1517bd21c9ee69c90e83a372
chaewonjeong <chaewonjeong.dev@gmail.com>
2024-06-21 11:43:17 +0900

    feat: 도메인 YoutubeSong 추가

---
+++ turntable/src/main/java/com/example/turntable/domain/YoutubeSong.java
@@ -0,0 +1,22 @@
+package com.example.turntable.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class YoutubeSong {
+    @Id
+    private Integer spotifySongId;
+
+    @Column(nullable = false)
+    private String youtubeUrl;
+}
