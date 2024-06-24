1ec059df5bf61cf68f8d6ad8a1dd6a3bdba10e1a
onegqueen <rkdwldnjs30@naver.com>
2024-06-24 16:04:15 +0900

    feat: ncp에 저장된 사진을 요청하는 컨트롤러 메소드 추가

--- turntable/src/main/java/com/example/turntable/controller/ncpController.java
+++ turntable/src/main/java/com/example/turntable/controller/ncpController.java
@@ -1,2 +1,33 @@
-package com.example.turntable.controller;public class ncpController {
+package com.example.turntable.controller;
+
+import com.example.turntable.service.NcpService;
+import java.io.IOException;
+import jdk.jfr.Registered;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ncpController {
+
+    @Autowired
+    private NcpService ncpService;
+
+    @GetMapping("image/{imageName}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String imageName) {
+        try {
+            byte[] imageData = ncpService.getImgObject(imageName);
+            return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\""+imageName+"\"")
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(imageData);
+        }catch (IOException e){
+            return ResponseEntity.status(500).body(null);
+        }
+    }
+
 }
