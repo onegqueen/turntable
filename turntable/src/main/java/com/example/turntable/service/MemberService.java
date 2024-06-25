0142d2c2d78218ef49636485ccba156df7561cb3
onegqueen <rkdwldnjs30@naver.com>
2024-06-25 14:58:55 +0900

    feat : member 서비스 및 멤버 정보 저장 메소드 추가

--- turntable/src/main/java/com/example/turntable/service/MemberService.java
+++ turntable/src/main/java/com/example/turntable/service/MemberService.java
@@ -1,2 +1,30 @@
-package com.example.turntable.service;public class MemberService {
+package com.example.turntable.service;
+
+import com.example.turntable.domain.Member;
+import com.example.turntable.repository.MemberRepository;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final NcpService ncpService;
+
+    @Transactional
+    public void create(String memberId,String nickname, String password, MultipartFile bgImg) throws IOException {
+        final Member member = Member.builder()
+            .name(memberId)
+            .nickname(nickname)
+            .password(password)
+            .backGroundImage(ncpService.uploadFile(bgImg))
+            .build();
+        memberRepository.save(member);
+    }
+
+
+
 }
