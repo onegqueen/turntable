9bc538074e0b0275f3bfd0995d15dcba0399a2ce
onegqueen <rkdwldnjs30@naver.com>
2024-06-27 14:25:02 +0900

    feat: 사용자 게시글 저장을 위한 repository 및 서비스 메소드 추가

--- turntable/src/main/java/com/example/turntable/repository/DailyCommentRepository.java
+++ turntable/src/main/java/com/example/turntable/repository/DailyCommentRepository.java
@@ -1,2 +1,8 @@
-package com.example.turntable.repository;public interface DailyCommentRepository {
+package com.example.turntable.repository;
+
+import com.example.turntable.domain.DailyComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DailyCommentRepository  extends JpaRepository<DailyComment, Long> {
+
 }
